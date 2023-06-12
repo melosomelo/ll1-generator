b1e019d2fb3eca35d379b4318c946be106c2603b
melosomelo <mateusmelornrn@gmail.com>
2023-06-12 12:06:47 -0300

    add entrypoint for library

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from "./buildGrammar";
+export * from "./first";
+export * from "./follow";
+export * from "./generateTable";
+export * from "./parse";
+export * from "./symbols";
+export { terminal, nonTerminal } from "./util";
