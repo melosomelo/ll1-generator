150ad9b27a8f347c3a89349639214a2a3f1da598
melosomelo <mateusmelornrn@gmail.com>
2023-06-13 10:58:04 -0300

    fix exports from entrypoint file

--- core/index.ts
+++ core/index.ts
@@ -1,7 +1,7 @@
-export * from "./src/buildGrammar";
-export * from "./src/first";
-export * from "./src/follow";
-export * from "./src/generateTable";
-export * from "./src/parse";
+export { default as buildGrammar } from "./src/buildGrammar";
+export { default as firstSet } from "./src/first";
+export { default as followSet } from "./src/follow";
+export { default as generateParseTable } from "./src/generateTable";
+export { default as parse } from "./src/parse";
 export * from "./src/symbols";
 export { terminal, nonTerminal } from "./src/util";
